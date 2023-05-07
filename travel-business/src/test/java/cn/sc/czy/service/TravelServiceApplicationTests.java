c82df82a9260bfd00344db0eef0f59bce9afd222
lingling-lynnchan <linglingright@outlook.com>
2023-05-07 23:58:11 +0800

    2023-05-07 23:58

--- travel-business/src/test/java/cn/sc/czy/service/TravelServiceApplicationTests.java
+++ travel-business/src/test/java/cn/sc/czy/service/TravelServiceApplicationTests.java
@@ -1,13 +1,25 @@
 package cn.sc.czy.service;
 
+import gaarason.database.contract.eloquent.Record;
+import gaarason.database.eloquent.GeneralModel;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
+import java.io.Serializable;
+import java.util.Map;
 
 @SpringBootTest
 class TravelServiceApplicationTests {
 
+    @Resource
+    GeneralModel generalModel;
+
     @Test
     void contextLoads() {
+        Record<GeneralModel.Table, Serializable> record=generalModel.newQuery().from("student").where("id",1).firstOrFail();
+        Map<String, Object> stringObjectMap = record.toMap();
+        System.out.println(stringObjectMap);
     }
 
 }
